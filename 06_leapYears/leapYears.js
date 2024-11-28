d0a6981461522b3e0af6cfed44baef686779a5ae
Swishy <swishyeth@gmail.com>
2024-11-28 18:33:44 +0530

    More Solutions

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
-
-};
-
+const leapYears = function(year) {
+    var remainder; remainder = year % 4; 
+    var remainder2 = year % 100; 
+    var remainder3 = year % 400;
+    
+    if (remainder !== 0 )
+    {return false}
+      else if (remainder2 === 0 && remainder3 !== 0)
+         {return false}
+          else {
+            return true
+               }
+  
+  };
 // Do not edit below this line
 module.exports = leapYears;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,22 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(F) {
+  var Ctemp = (F - 32) * 5/9;
+  if (Ctemp % 1 === 0 ){
+    return Ctemp
+  }
+  else {C = Ctemp.toFixed(1);
+  return parseFloat(C)}  
+
 };
 
-const convertToFahrenheit = function() {
+
+const convertToFahrenheit = function(C) {
+  Ftemp = (C * 9/5) + 32;
+  if ( Ftemp % 1 === 0){
+    return Ftemp
+  }
+  else {
+  F = Ftemp.toFixed(1);
+  return parseFloat(F)}
 };
 
 // Do not edit below this line
